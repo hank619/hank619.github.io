dc6739e663011048448ea279082d75259e0e85ad
zhanghong <hong.zhang@axinan.com>
2024-04-25 15:10:19 +0800

    feat: add local search

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -5,4 +5,30 @@
  */
 import { defineConfig } from "vitepress";
 
-export const shared = defineConfig({});
+export const shared = defineConfig({
+  themeConfig: {
+    search: {
+      provider: "local",
+      options: {
+        locales: {
+          zh: {
+            translations: {
+              button: {
+                buttonText: "搜索文档",
+                buttonAriaLabel: "搜索文档",
+              },
+              modal: {
+                noResultsText: "无法找到相关结果",
+                resetButtonTitle: "清除查询条件",
+                footer: {
+                  selectText: "选择",
+                  navigateText: "切换",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
