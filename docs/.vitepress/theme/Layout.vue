a17350fd5ecb66fc5706001f9ca14fc142f2c224
zhanghong <hong.zhang@axinan.com>
2024-04-28 18:25:37 +0800

    feat: giscus is YYDS, easy to integrate and clean, what's more, it's active with good maintin, so I choose it to replace gitalk

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -5,48 +5,33 @@
 -->
 <script setup lang="ts">
 import { onMounted, watch, nextTick } from "vue";
-import "gitalk/dist/gitalk.css";
-import Gitalk from "gitalk";
 import DefaultTheme from "vitepress/theme";
 import { useRouter } from "vitepress";
-import "./global.css";
+import Giscus from "@giscus/vue";
 
 const { Layout } = DefaultTheme;
 const { route } = useRouter();
-
-onMounted(() => {
-  watch(
-    () => route.path,
-    () => {
-      nextTick(() => {
-        if (typeof window !== "undefined") {
-          const contentDiv = document.querySelector(".content-container");
-          if (contentDiv) {
-            const oldGTDiv = document.getElementById("gitalk-page-container");
-            if (oldGTDiv) {
-              contentDiv.removeChild(oldGTDiv);
-            }
-            const newGTDiv = document.createElement("div");
-            newGTDiv.setAttribute("id", "gitalk-page-container");
-            contentDiv.appendChild(newGTDiv);
-            const gitalk = new Gitalk({
-              clientID: "50e2b0842c74106c4671",
-              clientSecret: "63c66cb4aba43932a1621a0a1420b9992cf580b2",
-              repo: "hank619.github.io",
-              owner: "hank619",
-              admin: ["hank619"],
-              id: location.pathname,
-              distractionFreeMode: false,
-            });
-            gitalk.render("gitalk-page-container");
-          }
-        }
-      });
-    }
-  );
-});
 </script>
 
 <template>
-  <Layout />
+  <Layout>
+    <template #doc-after>
+      <Giscus
+        id="comments"
+        repo="hank619/hank619.github.io"
+        repoId="R_kgDOLy7dmQ"
+        category="Announcements"
+        categoryId="DIC_kwDOLy7dmc4Ce_7U"
+        mapping="pathname"
+        term="Welcome to Hank's website"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="bottom"
+        theme="preferred_color_scheme"
+        lang="en"
+        loading="lazy"
+      />
+      <div style="height: 100px"></div>
+    </template>
+  </Layout>
 </template>
