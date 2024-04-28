87025a6b3b9b0d19e206e5eea60ceeb9790f2533
zhanghong <hong.zhang@axinan.com>
2024-04-28 16:57:31 +0800

    feat: add gitalk as comment vendor

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -4,32 +4,49 @@
  * @Description: 
 -->
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, watch, nextTick } from "vue";
 import "gitalk/dist/gitalk.css";
 import Gitalk from "gitalk";
 import DefaultTheme from "vitepress/theme";
+import { useRouter } from "vitepress";
 import "./global.css";
 
 const { Layout } = DefaultTheme;
+const { route } = useRouter();
 
 onMounted(() => {
-  const gitalk = new Gitalk({
-    clientID: "50e2b0842c74106c4671",
-    clientSecret: "63c66cb4aba43932a1621a0a1420b9992cf580b2",
-    repo: "hank619.github.io", // The repository of store comments,
-    owner: "hank619",
-    admin: ["hank619"],
-    id: location.pathname, // Ensure uniqueness and length less than 50
-    distractionFreeMode: false, // Facebook-like distraction free mode
-  });
-  gitalk.render("gitalk-container");
+  watch(
+    () => route.path,
+    () => {
+      nextTick(() => {
+        if (typeof window !== "undefined") {
+          const contentDiv = document.querySelector(".content-container");
+          if (contentDiv) {
+            const oldGTDiv = document.getElementById("gitalk-page-container");
+            if (oldGTDiv) {
+              contentDiv.removeChild(oldGTDiv);
+            }
+            const newGTDiv = document.createElement("div");
+            newGTDiv.setAttribute("id", "gitalk-page-container");
+            contentDiv.appendChild(newGTDiv);
+            const gitalk = new Gitalk({
+              clientID: "50e2b0842c74106c4671",
+              clientSecret: "63c66cb4aba43932a1621a0a1420b9992cf580b2",
+              repo: "hank619.github.io",
+              owner: "hank619",
+              admin: ["hank619"],
+              id: location.pathname,
+              distractionFreeMode: false,
+            });
+            gitalk.render("gitalk-page-container");
+          }
+        }
+      });
+    }
+  );
 });
 </script>
 
 <template>
-  <Layout>
-    <template #doc-bottom>
-      <div id="gitalk-container"></div>
-    </template>
-  </Layout>
+  <Layout />
 </template>
